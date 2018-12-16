768813bc0dbe864a600e89c4eded8252ebcc727e
Tomás Mansur <34463537+plantbasedalchemist@users.noreply.github.com>
2018-12-16 17:35:09 -0300

    Update get_ip.c

--- get_ip.c
+++ get_ip.c
@@ -8,9 +8,9 @@
 #include<arpa/inet.h>
 int main(int argc, char **argv)
 {
-		if (argc < 2 || argc > 2)
+	if (argc < 2 || argc > 2)
   {
-    fprintf(stderr, "Uso: %s link\nEjemplo: %s www.yahoo.com/lalal.html\n", argv[0], argv[0]);
+    fprintf(stderr, "Usage: %s link\nExample: %s www.yahoo.com/lalal.html\n", argv[0], argv[0]);
     return(0);
   }
   struct in_addr inp;
@@ -18,13 +18,13 @@ int main(int argc, char **argv)
   struct hostent *host;
   char url[strlen(argv[1])];
   strcpy(url,argv[1]);
-		char *host_name = strtok(url,"/");
+	char *host_name = strtok(url,"/");
   host = gethostbyname(host_name);
   if (host != NULL)
   {
     memcpy(&inp, host->h_addr_list[0], host->h_length);
     sprintf(ip, "%s", inet_ntoa(inp));
-				printf("IP ---> %s\n",ip);
+		printf("IP ---> %s\n",ip);
   }
   else
   {
